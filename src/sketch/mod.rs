403c4727346cd972e9bccde820ec3e3ab822ddd8
Grayson <russell.hay@gmail.com>
2024-04-21 11:59:22 -0700

    vybh1x7.1

--- src/sketch/mod.rs
+++ src/sketch/mod.rs
@@ -13,10 +13,10 @@ mod palettes;
 pub const WIDTH: u32 = 1200;
 pub const HEIGHT: u32 = WIDTH * 9 / 16;
 
-pub const STRIP_W: f32 = 400.0;
+pub const STRIP_W: f32 = 100.0;
 pub const STRIP_W2: f32 = STRIP_W * 2.0;
 
-pub const MAX_FREQ: f32 = 3.0;
+pub const MAX_FREQ: f32 = 10.0;
 
 pub struct Model {
     frequency: f32,
@@ -64,7 +64,8 @@ impl Model {
 
 impl Model {
     pub fn draw(&self, app: &App, draw: &Draw) {
-        (0..1000).map(|i| (i as f32) / 1000.0).for_each(|t| {
+        (0..2000).map(|i| (i as f32) / 2000.0).for_each(|t| {
+            let base = t * self.frequency * TAU;
             let x = lerp(0.0, WIDTH as f32, t);
             // TODO: Generate Palette
             self.palette
@@ -72,7 +73,7 @@ impl Model {
                 .zip(self.phases.iter())
                 .enumerate()
                 .for_each(|(idx, (color, phase))| {
-                    let y = ((t) * self.frequency * TAU).sin() * 100.0 * (t + *phase).cos();
+                    let y = base.sin() * 100.0 * (t + *phase).cos();
                     let offset = calc_offset(self.palette.len(), idx);
                     // let color = Hsla::new(
                     //     color.hue,
@@ -80,7 +81,14 @@ impl Model {
                     //     color.lightness,
                     //     (0.5 + (t * self.frequency * TAU + *phase).cos().abs()).clamp(0.25, 0.75),
                     // );
-                    Model::draw_section(draw, idx, x, y + offset, color.into_lin_srgba());
+                    Model::draw_section(
+                        draw,
+                        idx,
+                        base.cos() - *phase,
+                        x,
+                        y + offset,
+                        color.into_lin_srgba(),
+                    );
                 });
         });
 
@@ -103,15 +111,19 @@ impl Model {
         }
     }
 
-    fn draw_section<C>(draw: &Draw, count: usize, x: f32, y: f32, color: C)
+    fn draw_section<C>(draw: &Draw, count: usize, base: f32, x: f32, y: f32, color: C)
     where
         C: IntoLinSrgba<f32>,
     {
+        let angle = base + (count as f32) * TAU / (count as f32);
+        let x = x - angle.cos() * STRIP_W2;
+        let y = y - angle.sin() * STRIP_W2;
         draw.line()
             .start(pt2(x, y - STRIP_W))
             .end(pt2(x, y + STRIP_W))
             .color(color.into_lin_srgba())
-            .stroke_weight(2.0);
+            .stroke_weight(1.0);
+        // draw.ellipse().x_y(x, y).radius(5.0).color(RED);
     }
 }
 
--- src/sketch/palettes.rs
+++ src/sketch/palettes.rs
@@ -1,26 +1,30 @@
 use nannou::{color::Gradient, prelude::*};
 
 pub fn complement() -> Gradient<Hsl> {
-    let start = hsl(random_f32(), 0.5, random_f32());
+    let sat = random_f32() * 0.5 + 0.5;
+    let start = hsl(random_f32(), sat, random_f32() * 0.5 + 0.5);
     let end = hsl(
         (start.hue.to_positive_degrees() + 180.0) / 360.0,
-        0.5,
+        sat,
         random_f32() * 0.5 + 0.5,
     );
     Gradient::new(vec![start, end])
 }
 
 pub fn split_complement() -> Gradient<Hsl> {
-    let start = hsl(random_f32(), 0.5, random_f32() * 0.5 + 0.5);
+    let sat = random_f32() * 0.5 + 0.5;
+    let light = random_f32() * 0.5 + 0.25;
+
+    let start = hsl(random_f32(), sat, light);
     let mid = hsl(
         (start.hue.to_positive_degrees() + 150.0) / 360.0,
-        0.5,
-        random_f32() * 0.5 + 0.5,
+        start.saturation,
+        start.lightness * light,
     );
     let end = hsl(
         (start.hue.to_positive_degrees() + 210.0) / 360.0,
-        0.5,
-        random_f32() * 0.5 + 0.5,
+        start.saturation,
+        start.lightness * light,
     );
 
     Gradient::new(vec![start, mid, end])
